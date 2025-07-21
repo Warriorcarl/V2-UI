9a87fb4055ca2f1afa65f8cc311ebb90dc655390
warriorcarl <warriorcarl@yahoo.com>
2025-07-21 22:56:51 +0700

    first commit

---
+++ src/constants/index.ts
@@ -0,0 +1,161 @@
+import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
+import { AbstractConnector } from '@web3-react/abstract-connector'
+
+import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
+
+export const ROUTER_ADDRESS = '0xE25DE9776034cCbd2c4F58833e580182B254C676'
+
+// a list of tokens by chain
+type ChainTokenList = {
+  readonly [chainId in ChainId]: Token[]
+}
+
+export const DAI = new Token(ChainId.HELIOS, '0x634C01854FE561F413508648a4f63B70328d3d34', 18, 'DAI', 'test Dai')
+export const USDC = new Token(ChainId.HELIOS, '0x00389a8C2E1f2c7913e320dF568B0ba647c5f18C', 18, 'USDC', 'test USDC')
+export const USDT = new Token(ChainId.HELIOS, '0x00C325A985AB3c0ACd65511c783446707bBE2c30', 18, 'USDT', 'test USDT')
+export const COMP = new Token(ChainId.HELIOS, '0x91f75c859a6896ebd97Fae71eC5a0D85D5C9516b', 18, 'COMP', 'test Compound')
+export const MKR = new Token(ChainId.HELIOS, '0xd48dd8526322463A43f27ED261DB63AeadCa7033', 18, 'MKR', 'test Maker')
+export const AMPL = new Token(ChainId.HELIOS, '0x405AA9B08E5FD5e0E2B4297CDA10416777655ADe', 18, 'AMPL', 'test Ampleforth')
+export const BTC = new Token(ChainId.HELIOS, '0xA3b530412771ef981D2c126664bA0d180eca5Fa4', 18, 'BTC', 'test BTC')
+
+const WETH_ONLY: ChainTokenList = {
+  [ChainId.HELIOS]: [WETH[ChainId.HELIOS]],
+}
+
+// used to construct intermediary pairs for trading
+export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
+  ...WETH_ONLY,
+  [ChainId.HELIOS]: [...WETH_ONLY[ChainId.HELIOS], DAI, USDC, USDT, COMP, MKR]
+}
+
+/**
+ * Some tokens can only be swapped via certain pairs, so we override the list of bases that are considered for these
+ * tokens.
+ */
+export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: Token[] } } = {
+  [ChainId.HELIOS]: {
+    [AMPL.address]: [DAI, WETH[ChainId.HELIOS]]
+  }
+}
+
+// used for display in the default list when adding liquidity
+export const SUGGESTED_BASES: ChainTokenList = {
+  ...WETH_ONLY,
+  [ChainId.HELIOS]: [...WETH_ONLY[ChainId.HELIOS], DAI, USDC, USDT]
+}
+
+// used to construct the list of all pairs we consider by default in the frontend
+export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
+  ...WETH_ONLY,
+  [ChainId.HELIOS]: [...WETH_ONLY[ChainId.HELIOS], DAI, USDC, USDT]
+}
+
+export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
+  [ChainId.HELIOS]: [
+    [
+      new Token(ChainId.HELIOS, '0x1Fe39aDDA45db93cfEA4a4794614CbBe3e6713Dd', 18, 'cDAI', 'test Compound Dai'),
+      new Token(ChainId.HELIOS, '0x4a1B70FBcA1CF40AFc5a61Dd2306aC8bf0b65eEC', 18, 'cUSDC', 'test Compound USD Coin')
+    ],
+    [USDC, USDT],
+    [DAI, USDT]
+  ]
+}
+
+export interface WalletInfo {
+  connector?: AbstractConnector
+  name: string
+  iconName: string
+  description: string
+  href: string | null
+  color: string
+  primary?: true
+  mobile?: true
+  mobileOnly?: true
+}
+
+export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
+  INJECTED: {
+    connector: injected,
+    name: 'Injected',
+    iconName: 'arrow-right.svg',
+    description: 'Injected web3 provider.',
+    href: null,
+    color: '#010101',
+    primary: true
+  },
+  METAMASK: {
+    connector: injected,
+    name: 'MetaMask',
+    iconName: 'metamask.png',
+    description: 'Easy-to-use browser extension.',
+    href: null,
+    color: '#E8831D'
+  },
+  WALLET_CONNECT: {
+    connector: walletconnect,
+    name: 'WalletConnect',
+    iconName: 'walletConnectIcon.svg',
+    description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
+    href: null,
+    color: '#4196FC',
+    mobile: true
+  },
+  WALLET_LINK: {
+    connector: walletlink,
+    name: 'Coinbase Wallet',
+    iconName: 'coinbaseWalletIcon.svg',
+    description: 'Use Coinbase Wallet app on mobile device',
+    href: null,
+    color: '#315CF5'
+  },
+  COINBASE_LINK: {
+    name: 'Open in Coinbase Wallet',
+    iconName: 'coinbaseWalletIcon.svg',
+    description: 'Open in Coinbase Wallet app.',
+    href: 'https://go.cb-w.com/mtUDhEZPy1',
+    color: '#315CF5',
+    mobile: true,
+    mobileOnly: true
+  },
+  FORTMATIC: {
+    connector: fortmatic,
+    name: 'Fortmatic',
+    iconName: 'fortmaticIcon.png',
+    description: 'Login using Fortmatic hosted wallet',
+    href: null,
+    color: '#6748FF',
+    mobile: true
+  },
+  Portis: {
+    connector: portis,
+    name: 'Portis',
+    iconName: 'portisIcon.png',
+    description: 'Login using Portis hosted wallet',
+    href: null,
+    color: '#4A6C9B',
+    mobile: true
+  }
+}
+
+export const NetworkContextName = 'NETWORK'
+
+// default allowed slippage, in bips
+export const INITIAL_ALLOWED_SLIPPAGE = 50
+// 20 minutes, denominated in seconds
+export const DEFAULT_DEADLINE_FROM_NOW = 60 * 20
+
+// one basis point
+export const ONE_BIPS = new Percent(JSBI.BigInt(1), JSBI.BigInt(10000))
+export const BIPS_BASE = JSBI.BigInt(10000)
+// used for warning states
+export const ALLOWED_PRICE_IMPACT_LOW: Percent = new Percent(JSBI.BigInt(100), BIPS_BASE) // 1%
+export const ALLOWED_PRICE_IMPACT_MEDIUM: Percent = new Percent(JSBI.BigInt(300), BIPS_BASE) // 3%
+export const ALLOWED_PRICE_IMPACT_HIGH: Percent = new Percent(JSBI.BigInt(500), BIPS_BASE) // 5%
+// if the price slippage exceeds this number, force the user to type 'confirm' to execute
+export const PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN: Percent = new Percent(JSBI.BigInt(1000), BIPS_BASE) // 10%
+// for non expert mode disable swaps above this
+export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(JSBI.BigInt(1500), BIPS_BASE) // 15%
+
+// used to ensure the user doesn't send so much ETH so they end up with <.01
+export const MIN_ETH: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 ETH
+export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.BigInt(10000))
